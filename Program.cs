1b4add8e7baea5c93b6bcf0250fa732f2af12376
yaroslav <ruzmanova@mail.ru>
2022-11-20 13:23:14 +0300

    задача 1, часть 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// часть 2. Написать программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше.
+
+int a = -9 , b = -3 ;
+int max = b ;
+int min = a ;
+
+if (b > a) max = b ;
+if (b > a) min = a ;
+
+Console.Write ("max = ") ;
+Console.WriteLine ( max ) ;
+
+Console.Write ("min =") ;
+Console.WriteLine ( min ) ;
