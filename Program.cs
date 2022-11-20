f2b487bcbe7615dd265ac858c7201355dd43a53c
yaroslav <ruzmanova@mail.ru>
2022-11-20 14:15:17 +0300

    задача 2, часть 2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//  Часть 2. Написать программу, которая принимает на вход 3 числа и выдает максимальное из этих чисел
+
+int a = 44 ;
+int b = 5 ;
+int c = 78 ;
+
+int max = a ;
+
+if ( a > max ) max = a ;
+if ( b > max ) max = b ;
+if ( c > max ) max = c ;
+
+Console.Write ("max=") ;
+Console.WriteLine ( max ) ;
